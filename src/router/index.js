19976046ff81bc9957bdf2979155917af6e74606
eli9000 <eli9000@gmail.com>
2018-09-04 05:38:32 -0600

    Setting up and tindereng

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Test from "@/components/Test";
+import Users from "@/components/Users";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: __dirname,
+  routes: [
+    {
+      path: "/",
+      name: "Users",
+      component: Users
+    },
+    {
+      path: "/test",
+      component: Test
+    }
+  ]
+});
